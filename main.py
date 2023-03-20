9a36194a8494a7c908a7ce39b00ad61a599cefdf
Joseph Campbell <jsc940@gmail.com>
2023-03-19 23:04:20 -0600

    Store words in a different structure

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import random
 import requests
 import json
 from playwright.sync_api import Playwright, sync_playwright
+import os
 
 def fetch_words(num_words):
     """
-    Fetches a list of random words using Datamuse API.
+    Fetches a list of words ordered by length using Datamuse API.
     """
-    url = f"https://api.datamuse.com/words?sp=*&max=1000"
+    url = f"https://api.datamuse.com/words?sp=*&max=1000&orderby=length"
     response = requests.get(url)
     data = response.json()
-    words = [word['word'] for word in data][:num_words]
-    return words
+    words = [word['word'] for word in data]
+    return words[:num_words]
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
@@ -20,14 +22,14 @@ with sync_playwright() as playwright:
     page.goto('https://www.accessidaho.org/itd/driver/plates/plate/85')
 
     # Load the existing list of words from the JSON file, if it exists
-    try:
-        with open('random_words.json', 'r') as f:
+    if os.path.isfile('license_plates.json') and os.path.getsize('license_plates.json') > 0:
+        with open('license_plates.json', 'r') as f:
             words = json.load(f)
-    except FileNotFoundError:
-        words = []
+    else:
+        words = {"available": [], "unavailable": []}
 
     # Set the number of words to fetch and insert
-    num_words = 5
+    num_words = 15
 
     # Initialize a variable to store whether the element exists
     plate_not_available = False
@@ -39,23 +41,33 @@ with sync_playwright() as playwright:
         plate_input = page.locator('input[type="text"]')
         plate_input.fill(word)
 
+        page.wait_for_load_state()
+
         page.click("text=Generate Preview")
 
-        page.wait_for_timeout(1000)
+        page.wait_for_load_state()
+        page.wait_for_timeout(2000)
 
         page.click("text=Check Availability")
         
-        # Check if the error element exists
-        page.wait_for_timeout(1000)
-        
+        page.wait_for_load_state()
+        page.wait_for_timeout(2000)
+
         plate_available = page.locator("#available").is_visible()
 
         if plate_available:
-            words.append(word)
-            print(f"{word} is available")
+            if word not in words["available"]:
+                words["available"].append(word)
+                print(f"{word} is available")
+            else:
+                print(f"{word} was already in the list of available words")
             try_a_new_plate_button = page.locator("#available").get_by_text("Try a New Plate Message")
         else:
-            print(f"{word} is unavailable")
+            if word not in words["unavailable"]:
+                words["unavailable"].append(word)
+                print(f"{word} is unavailable")
+            else:
+                print(f"{word} was already in the list of unavailable words")
             try_a_new_plate_button = page.locator("#unavailable").get_by_text("Try a New Plate Message")
 
         try_a_new_plate_button.click()
@@ -63,5 +75,5 @@ with sync_playwright() as playwright:
     browser.close()
 
 # Append the new list of random words and error status to the existing JSON file.
-with open('available_words.json', 'w') as f:
+with open('license_plates.json', 'w') as f:
     json.dump(words, f, indent=4)
