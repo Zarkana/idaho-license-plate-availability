a82a30080d5822316cb826670d22ecb61f34f6b6
Joseph Campbell <jsc940@gmail.com>
2023-04-07 16:46:40 -0600

    Improve stability

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import json
 import os
 from playwright.sync_api import Playwright, sync_playwright
 
+def wait_for_one_to_be_visible(selector):
+    # Wait for one or more and then find the one that is visible
+    elements = page.locator(selector).all()
+    visible_element_count = len([element for element in elements if element.is_visible()])
+    while visible_element_count == 0:
+        elements = page.locator(selector).all()
+        visible_element_count = len([element for element in elements if element.is_visible()])
 
 def fetch_words():
     """Reads a list of words from a JSON file."""
@@ -9,6 +16,47 @@ def fetch_words():
         words = json.load(f)
     return words
 
+def enter_word(word):
+    global current_word
+    if word not in words["available"] and word not in words["unavailable"]:
+        current_word = word
+        plate_input = page.locator('input[type="text"]')
+        plate_input.fill(word)
+
+        page.click("text=Generate Preview")
+    else:
+        print(f"{word} was already in the list of license plates")
+        current_word = enter_word(words_to_check.pop())
+
+    # Write the current list of words and error status to the file after each word check
+    with open('license_plates.json', 'w') as f:
+        json.dump(words, f, indent=4)
+
+def handle_request(route, request):
+    if 'https://www.accessidaho.org/itd/driver/plates/checkAvailability' in request.url:
+        route.continue_()
+        global current_word
+        word = current_word
+        
+        wait_for_one_to_be_visible('.modal-dialog')
+
+        plate_available = page.locator("#available .modal-dialog").is_visible()
+        if plate_available:
+            words["available"].append(word)
+            print(f"{word} is available")
+            # try_a_new_plate_button = page.locator("#available").get_by_text("Try a New Plate Message")
+            try_a_new_plate_button = page.locator('//*[@id="available"]/div/div/div[3]/button')
+        else:
+            words["unavailable"].append(word)
+            print(f"{word} is unavailable")
+            # try_a_new_plate_button = page.locator("#unavailable").get_by_text("Try a New Plate Message")
+            try_a_new_plate_button = page.locator('//*[@id="unavailable"]/div/div/div[3]/button')
+
+        try_a_new_plate_button.click()
+        enter_word(words_to_check.pop())
+    elif 'https://www.accessidaho.org/itd/driver/plates/check' in request.url:
+        route.continue_()
+        page.click("text=Check Availability")
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
@@ -27,40 +75,12 @@ with sync_playwright() as playwright:
     # Fetch the list of words to insert
     words_to_check = fetch_words()
 
-    for word in words_to_check:
-        if word not in words["available"] and word not in words["unavailable"]:
-            plate_input = page.locator('input[type="text"]')
-            plate_input.fill(word)
+    page.route('**/*', lambda route, request: handle_request(route, request))
 
-            page.wait_for_load_state()
+    current_word = ""
+    enter_word(words_to_check.pop())
 
-            page.click("text=Generate Preview")
 
-            page.wait_for_load_state()
-            page.wait_for_timeout(2000)
-
-            page.click("text=Check Availability")
-            
-            page.wait_for_load_state()
-            page.wait_for_timeout(2000)
-
-            plate_available = page.locator("#available").is_visible()
-
-            if plate_available:
-                words["available"].append(word)
-                print(f"{word} is available")
-                try_a_new_plate_button = page.locator("#available").get_by_text("Try a New Plate Message")
-            else:
-                words["unavailable"].append(word)
-                print(f"{word} is unavailable")
-                try_a_new_plate_button = page.locator("#unavailable").get_by_text("Try a New Plate Message")
-
-            try_a_new_plate_button.click()
-        else:
-            print(f"{word} was already in the list of license plates")
-
-        # Write the current list of words and error status to the file after each word check
-        with open('license_plates.json', 'w') as f:
-            json.dump(words, f, indent=4)
+    page.wait_for_timeout(200000)
 
     browser.close()
